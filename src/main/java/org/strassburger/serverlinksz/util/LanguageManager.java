9927935b35dc1984faf5167f8c5e602025864e8d
KartoffelChips <jan@strassburger.org>
2024-10-13 16:16:19 +0200

    added russion to lang manager

--- src/main/java/org/strassburger/serverlinksz/util/LanguageManager.java
+++ src/main/java/org/strassburger/serverlinksz/util/LanguageManager.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class LanguageManager {
     private final JavaPlugin plugin = ServerLinksZ.getInstance();
-    public static final List<String> defaultLangs = List.of("en-US", "de-DE", "zh-CN");
+    public static final List<String> defaultLangs = List.of("en-US", "de-DE", "zh-CN", "ru-RU");
 
     private HashMap<String, String> translationMap;
     private FileConfiguration langConfig;
